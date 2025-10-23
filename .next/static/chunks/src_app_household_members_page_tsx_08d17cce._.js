0176fd9d9531fab96633fc005ef3815d2f6fa64e
lucas mugi <lucasmugi499@gmail.com>
2025-10-23 17:15:14 +0000

    Error: ./src/lib/household/useHousehold.ts:431:37 Parsing ecmascript sou

--- .next/static/chunks/src_app_household_members_page_tsx_08d17cce._.js
+++ .next/static/chunks/src_app_household_members_page_tsx_08d17cce._.js
@@ -1,8 +1,8 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/node_modules_3691ab6e._.js",
-  "static/chunks/src_components_layout_page-header_tsx_734a38ec._.js"
+  "static/chunks/src_2ad3d37f._.js",
+  "static/chunks/node_modules_7b3203de._.js"
 ],
     source: "dynamic"
 });
