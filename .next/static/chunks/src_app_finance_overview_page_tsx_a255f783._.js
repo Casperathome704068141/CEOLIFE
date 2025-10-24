31d70b7e9aa2b4467b5dc8dd247af17593fdf148
lucas mugi <lucasmugi499@gmail.com>
2025-10-24 00:24:05 +0000

    i notice the beno page and the simulations pages are the same, this is t

---
+++ .next/static/chunks/src_app_finance_overview_page_tsx_a255f783._.js
@@ -0,0 +1,12 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_motion-dom_dist_es_ed5d7820._.js",
+  "static/chunks/node_modules_framer-motion_dist_es_c4413217._.js",
+  "static/chunks/node_modules_lodash_1d760160._.js",
+  "static/chunks/node_modules_recharts_es6_ff9e0454._.js",
+  "static/chunks/node_modules_165fe184._.js",
+  "static/chunks/src_components_7246387a._.js"
+],
+    source: "dynamic"
+});
