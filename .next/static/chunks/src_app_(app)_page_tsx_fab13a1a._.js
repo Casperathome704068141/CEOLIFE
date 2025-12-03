ff322426e735cd11ccd8dab5da6d361934fb4060
lucas mugi <lucasmugi499@gmail.com>
2025-12-03 15:50:14 +0000

    theres no way to go to other pages here

--- .next/static/chunks/src_app_(app)_page_tsx_fab13a1a._.js
+++ .next/static/chunks/src_app_(app)_page_tsx_fab13a1a._.js
@@ -1,8 +1,8 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/src_efa35cde._.js",
-  "static/chunks/node_modules_0e7a213f._.js"
+  "static/chunks/src_7c4dc81b._.js",
+  "static/chunks/node_modules_abec39dc._.js"
 ],
     source: "dynamic"
 });
